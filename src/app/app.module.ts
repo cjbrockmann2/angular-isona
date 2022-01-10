96638a30d64a709cfc531d459b14709c8c7e0efa
cjbrockmann2 <cjbrockmann@shared-files.de>
2022-01-10 13:10:27 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 // import {
 //   MatToolbarModule,
@@ -37,7 +38,7 @@ import { AppMainComponent } from './app-main/app-main.component';
     MatListModule,
     MatButtonModule,
     MatIconModule,
-
+    FlexLayoutModule,
     /* FontAwesomeModule, */
   ],
   declarations: [
