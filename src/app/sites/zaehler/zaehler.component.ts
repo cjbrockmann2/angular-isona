156f0e7621299e77faf6ffacb4129270c8ce958c
cjbrockmann2 <cjbrockmann@shared-files.de>
2022-01-10 16:01:18 +0100

    1 file modified, 1 added

--- src/app/sites/zaehler/zaehler.component.ts
+++ src/app/sites/zaehler/zaehler.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-zaehler',
   templateUrl: './zaehler.component.html',
-  styleUrls: ['./zaehler.component.css']
+  styleUrls: ['./zaehler.component.css'],
 })
 export class ZaehlerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
