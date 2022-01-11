6077885435a879b2eb8113e67ecc2a65c27e8091
cjbrockmann2 <cjbrockmann@shared-files.de>
2022-01-11 10:30:40 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,11 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 /* import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; */
 
 import { AppComponent } from './app.component';
@@ -37,6 +41,8 @@ import { AppMainComponent } from './app-main/app-main.component';
     MatListModule,
     MatButtonModule,
     MatIconModule,
+    MatMenuModule,
+    MatExpansionModule,
     FlexLayoutModule,
     /* FontAwesomeModule, */
   ],
